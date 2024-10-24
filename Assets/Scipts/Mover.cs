d85238ef318ac9217597cfe94fda4a10330abcc2
reisuj <reisuj@gmail.com>
2024-10-24 19:29:50 -0400

    Added basic keyboard movement

--- Assets/Scipts/Mover.cs
+++ Assets/Scipts/Mover.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 10.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,10 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(.001f, 0, 0);
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float yValue = 0;
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(xValue, yValue, zValue);
     }
 }
